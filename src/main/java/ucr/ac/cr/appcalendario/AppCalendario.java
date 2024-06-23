eca6c7b9500cb35ed89481c7e54bc88a22c4ef41
Gretwix <76453311+Gretwix@users.noreply.github.com>
2024-06-23 17:14:06 -0600

    plan

--- src/main/java/ucr/ac/cr/appcalendario/AppCalendario.java
+++ src/main/java/ucr/ac/cr/appcalendario/AppCalendario.java
@@ -1,7 +1,8 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
+ 
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+*/
 package ucr.ac.cr.appcalendario;
 
 import controller.CoursesJpaController;
@@ -9,8 +10,9 @@ import controller.LoginController;
 import controller.MainController;
 import controller.MajorsJpaController;
 import controller.PlanJpaController;
-import controller.RegistroController;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import javax.persistence.Persistence;
 import modelo.Courses;
 import modelo.Majors;
 import modelo.Plan;
@@ -18,16 +20,37 @@ import modelo.User;
 
 /**
  *
- * @author alehe
- */
+ 
+@author alehe*/
 public class AppCalendario {
 
     public static void main(String[] args) {
          // Crea un ArrayList para almacenar los usuarios registrados
-       
+
 
         // Pasa el ArrayList a LoginController
-         LoginController loginController = new LoginController();
-//     
+         //LoginController loginController = new LoginController();
+         try{
+             PlanJpaController planJPA = new PlanJpaController();
+            int startDateInt = 20240203;  // Formato YYYYMMDD
+            int endDateInt = 20240104;    // Formato YYYYMMDD
+            LocalDate startDate = intToLocalDate(startDateInt);
+            LocalDate endDate = intToLocalDate(endDateInt);
+            String careerCode = "aa";
+
+        Plan plan = new Plan(1, startDate, "jeikol@hotmail.com", 1, endDate);
+            planJPA.create(plan);
+         //planJPA.findPlan(12);
+
+    }catch(Exception ex){
+    System.err.println("Error al agregar."+ ex.getMessage());
+    }
+//
+    }
+    public static LocalDate intToLocalDate(int dateInt) {
+        int year = dateInt / 10000;
+        int month = (dateInt % 10000) / 100;
+        int day = dateInt % 100;
+        return LocalDate.of(year, month, day);
     }
 }
